bc829a0612f2b867460c5f1b184367c24f95d940
Динис Сатвалов <drkappa@mail.ru>
2021-07-06 13:11:22 +0500

    test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using ProgramSettings;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -16,32 +17,40 @@ namespace ConsoleApp3
 {
     public class Program
     {
-        class test
+        class User
         {
-            public long migration { get; set; }
-            public test(long tmp)
+            public long chatId { get; set; }            //айди чата 
+            public long migration { get; set; }         //значение миграции в чате с chatId
+            public bool setValueCheck { get; set; }     //состояние указывающее что нужно принять значение migration
+            public  Message setterValueId { get; set; } //Message в котором хранится id пользователя запустившего команду set_migration
+            public User(long tmp, long Id, bool check, Message msg)
             {
+                chatId = Id;
                 migration = tmp;
+                setValueCheck = check;
+                setterValueId = msg;
+            }
+            public User()
+            {
             }
         }
 
         private static TelegramBotClient Bot;
-        static test tmp;
-        const  string path = "gg.json";
-        public static long mig = 0;
-        public static bool setValueCheck = false;
-        public static Message setterValueId;
+
+        static long tmpChatId;                          //класс для работы с json файлом
+        const string path = "gg.json";                  //путь к файлу json
+
+        static User user = new User();                  //буффераня переменная, в которой будех храниться информация о чате
+        static List<User> Users = new List<User>();     //лист, в котором хранится информация о чате
 
 
         public static async Task Main()
         {
-            tmp = JsonConvert.DeserializeObject<test>(System.IO.File.ReadAllText(path));
-            mig = tmp.migration;
             Bot = new TelegramBotClient(Configuration.BotToken);
             var me = await Bot.GetMeAsync();
             var cts = new CancellationTokenSource();
-
-            Bot.StartReceiving(new DefaultUpdateHandler(HandleUpdateAsync, HandleErrorAsync), cts.Token);
+            Users = JsonConvert.DeserializeObject<List<User>>(System.IO.File.ReadAllText(path));
+            Bot.StartReceiving(new DefaultUpdateHandler(HandleUpdateAsync, HandleErrorAsync), cts.Token);       //подключаем обработчик на обновления и ошибки
 
             Console.WriteLine($"Start listening for @{me.Username}");
             Console.ReadLine();
@@ -49,11 +58,14 @@ namespace ConsoleApp3
             cts.Cancel();
         }
 
+        // обработчик на события чата
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
+
+            //switch переключатель между задачами/апдейтами
             var handler = update.Type switch
             {
-                UpdateType.Message => BotOnMessageReceived(update,update.Message, cancellationToken),
+                UpdateType.Message => BotOnMessageReceived(update, update.Message, cancellationToken),
                 UpdateType.EditedMessage => BotOnMessageReceived(update, update.EditedMessage, cancellationToken),
                 _ => UnknownUpdateHandlerAsync(update)
             };
@@ -68,89 +80,139 @@ namespace ConsoleApp3
             }
         }
 
+        // метод отвечающий за обработку входящего сообщения
         private static async Task BotOnMessageReceived(Update update, Message message, CancellationToken cancellationToken)
-        {            
+        {         
+            tmpChatId = update.Message.Chat.Id;
+            await CheckUser();
+
             Console.WriteLine($"Receive message type: {message.Type}");
             Message sentMessage = message;
+
+            //проверяем является ли сообщенией командой 
+            user = Users.Where(n => n.chatId == tmpChatId).FirstOrDefault();
             if (message.Type == MessageType.Text && message.Text.StartsWith('/'))
             {
                 var action = (message.Text.Split('@').First()) switch
                 {
-                    "/showvalue" => ShowMigration(message),
-                    "/nextvalue" => NextMigration(message),
-                    "/setvalue" => GetValue(message),
+                    "/show_migration" => ShowMigration(message),
+                    "/next_migration" => NextMigration(message),
+                    "/set_migration" => GetValue(message),
                     _ => Usage(message)
                 };
                 sentMessage = await action;
-            }
-            else if (setValueCheck && message.Text.StartsWith('№'))
+            }// или это сообщение для ввода после метод setvalue
+            else if (message.Type == MessageType.Text && user.setValueCheck)
             {
-                var admin = Bot.GetChatMemberAsync(update.Message.Chat.Id, update.Message.From.Id, cancellationToken).Result;
-                if (setterValueId.From == message.From && (admin.Status != ChatMemberStatus.Administrator || admin.Status != ChatMemberStatus.Creator))
+                if (message.Text.StartsWith('№'))
                 {
-                    Console.WriteLine($"setvaluecheck - {setValueCheck}");
-                    Console.WriteLine("зашел с сообщением - " + message.Text + " " + message.From + " " + admin.Status);
-                    if (long.TryParse(message.Text.TrimStart('№'), out long temp))
+                    var admin = Bot.GetChatMemberAsync(update.Message.Chat.Id, update.Message.From.Id, cancellationToken).Result;
+                    
+                    if (user.setterValueId.From == message.From && user.setterValueId.Chat.Id == message.Chat.Id && (admin.Status != ChatMemberStatus.Administrator || admin.Status != ChatMemberStatus.Creator))
                     {
-                        mig = temp;
-                        await Bot.SendTextMessageAsync(chatId: message.Chat.Id, text: $"Последняя миграция - {mig}",
-                                                     replyMarkup: new ReplyKeyboardRemove());
-                        string json = JsonConvert.SerializeObject(new test(mig));
-                        using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
-                        {
-                            sw.WriteLine(json);
-                        }
-                    }
-                    else
-                    {
-                        await Bot.SendTextMessageAsync(message.From.Id, "Не удалось записать значение", replyMarkup: new ReplyKeyboardRemove());
-                    }                   
-                }
-                setValueCheck = false;
-                return;
+                      
+                        Console.WriteLine("зашел с сообщением - " + message.Text + " " + message.From + " " + admin.Status);
 
+                        await setValue(message);
+                    }
+                }
+                else
+                {
+                    await Bot.SendTextMessageAsync(message.Chat.Id, "Не удалось записать значение", replyMarkup: new ReplyKeyboardRemove());
+                    user.setValueCheck = false;
+                }
+
+            }
+
+            await SaveUsers();
+            Console.WriteLine($"The message was sent with id: {sentMessage.MessageId}");
+        }
+
+        //сохранений изменений состояния чата
+        static async Task SaveUsers()
+        {
+            string jsonPath = JsonConvert.SerializeObject(Users);
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(jsonPath);
+            }
+        }
+
+        // Проверка при первом запуске бота в чате
+        static async Task CheckUser()
+        {
+            if (Users.Count == 0)
+            {
+                Users.Add(new User(0, tmpChatId, false, null));
+                await SaveUsers();
+            }
+            else if (Users.Where(n => n.chatId == tmpChatId).FirstOrDefault() != null)
+            {
+                Users = JsonConvert.DeserializeObject<List<User>>(System.IO.File.ReadAllText(path));
+                Console.WriteLine("Chat id - " + tmpChatId);
             }
             else
             {
-                setValueCheck = false;
+                Users.Add(new User(0, tmpChatId, false, null));
+                await SaveUsers();
             }
-            Console.WriteLine($"The message was sent with id: {sentMessage.MessageId}");            
         }
 
+        //обработка состояния чата после записи значения миграции
+        public static async Task<Message> setValue(Message message)
+        {
+            if (long.TryParse(message.Text.TrimStart('№'), out long temp))
+            {
+                user.migration = temp;
+                user.setValueCheck = false;
+                await SaveUsers();
+                return await Bot.SendTextMessageAsync(chatId: message.Chat.Id, text: $"Последняя миграция - {user.migration}",
+                                             replyMarkup: new ReplyKeyboardRemove()); ;
+            }
+            else
+            {
+
+                user.setValueCheck = false;
+                return await Bot.SendTextMessageAsync(message.Chat.Id, "Не удалось записать значение", replyMarkup: new ReplyKeyboardRemove()); ;
+            }
+        }
+
+        //установка состояния чата на запись значения миграции
         public static async Task<Message> GetValue(Message message)
         {
-            setValueCheck = true;
-            setterValueId = message;
-            Console.WriteLine($"setvaluecheck - {setValueCheck}");
-            return await Bot.SendTextMessageAsync(message.From.Id, "Введите значение миграции (№.....)", replyMarkup: new ReplyKeyboardRemove());
+            user.setValueCheck = true;
+            user.setterValueId = message;
+            Console.WriteLine($"setvaluecheck - {user.setValueCheck}");
+            return await Bot.SendTextMessageAsync(message.Chat.Id, "Введите значение миграции (№.....)", replyMarkup: new ReplyKeyboardRemove());
         }
 
+        //получение следующего знечения миграции
         static async Task<Message> NextMigration(Message message)
         {
-            mig++;
-            string json = JsonConvert.SerializeObject(new test(mig));
-            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
-            {
-                sw.WriteLine(json);
-            }
+            user.migration++;
+            await SaveUsers();
             return await Bot.SendTextMessageAsync(chatId: message.Chat.Id,
-                                                       text: $"Следующая миграция - {mig}",
+                                                       text: $"Следующая миграция - {user.migration}",
                                                        replyMarkup: new ReplyKeyboardRemove());
         }
 
+        //вывод текущео значения миграции 
         static async Task<Message> ShowMigration(Message message)
         {
             return await Bot.SendTextMessageAsync(chatId: message.Chat.Id,
-                                                      text: $"Последняя миграция - {mig}",
+                                                      text: $"Последняя миграция - {user.migration}",
                                                       replyMarkup: new ReplyKeyboardRemove());
         }
 
+        //заглушка на обработчик событий в методе BotOnMessageReceived в switch
         static async Task<Message> Usage(Message message)
         {
             return await Bot.SendTextMessageAsync(chatId: message.Chat.Id,
                                                   text: "");
         }
 
+
+        //обработка исключений
         public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
             var ErrorMessage = exception switch
@@ -161,7 +223,7 @@ namespace ConsoleApp3
 
             Console.WriteLine(ErrorMessage);
             return Task.CompletedTask;
-        }     
+        }
         private static Task UnknownUpdateHandlerAsync(Update update)
         {
             Console.WriteLine($"Unknown update type: {update.Type}");
@@ -169,5 +231,3 @@ namespace ConsoleApp3
         }
     }
 }
-
-
